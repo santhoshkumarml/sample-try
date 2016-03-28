9cff5316133385eb54861af5e46aeeaf746ab86c
Santhosh Kumar <santhoshkumarml@gmail.com>
2016-03-27 21:08:28 -0700

    Remove repeated source files

--- sample-cpp/bin/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "utility.h"
-
-int main(int argc, char ** argv)
-{
-	PrintMessage printer("Hello World!!!");
-	printer.printMsg();
-	return 0;
-}
--- sample-cpp/bin/utility.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-class PrintMessage {
-private:
-	const char* msg;
-public:
-  PrintMessage(const char* msg);
-  void printMsg();
-};
